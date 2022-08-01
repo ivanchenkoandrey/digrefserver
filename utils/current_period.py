09c0a7644aa0c75509faac4caa15c402f80757ff
Andrey Ivanchenko <varioustemp@gmail.com>
2022-08-01 09:53:48 +0300

    period query fix

--- utils/current_period.py
+++ utils/current_period.py
@@ -7,5 +7,5 @@ from django.utils import timezone
 def get_current_period():
     now = timezone.now()
     current_period = Period.objects.filter(
-        Q(start_date__lt=now) & Q(end_date__gt=now)).first()
+        Q(start_date__lte=now) & Q(end_date__gte=now)).first()
     return current_period
