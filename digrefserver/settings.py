ea537c16ee99e2ae0412fb3c5ef7202c24d8d6f3
Andrey Ivanchenko <varioustemp@gmail.com>
2022-08-17 11:58:01 +0300

    django-cleanup added

--- digrefserver/settings.py
+++ digrefserver/settings.py
@@ -26,7 +26,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'auth_app.apps.AuthAppConfig',
-    'corsheaders'
+    'corsheaders',
+    'django_cleanup.apps.CleanupConfig',
 ]
 
 MIDDLEWARE = [
