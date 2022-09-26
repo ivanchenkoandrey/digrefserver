fbb9b8594bc8ed16bc82fe229ea330adc0e8944f
Andrey Ivanchenko <varioustemp@gmail.com>
2022-09-26 23:43:30 +0300

    challenges added, local fixes

---
+++ utils/challenge_algos.py
@@ -0,0 +1,7 @@
+fix_size_prize_logic = {
+    "is_active": True,
+    "name": "simple fixed",
+    "description": "distribution until fund is not 0",
+    "calculation_type": "auto",
+    "reward_time": "summarizing"
+}
