6747734443394a3155347dfbcbba518ece34f9cc
Talgat Bektleuov <t.bektleuov@innopolis.university>
2022-09-28 14:49:32 +0300

    Finished /create-challenge

--- auth_app/challenges_views/serializers.py
+++ auth_app/challenges_views/serializers.py
@@ -15,27 +15,21 @@ class CreateChallengeSerializer(serializers.ModelSerializer):
         validated_data['states'] = ['P']
         validated_data['challenge_mode'] = ['P']
 
-        name = validated_data.get('name')
-        description = validated_data.get('description')
-        end_at = validated_data.get('end_at')
         start_balance = validated_data.get('start_balance')
         parameters = validated_data.get('parameters')
 
         if parameters is None:
-            arr = [{"id": 2, "value": 5},
-                   {"id": 1, "value": start_balance // 5}]
-        elif parameters.get("id") == 2:
-            arr = [{"id": 2, "value": parameters.get("value")},
-                   {"id": 1, "value": start_balance // parameters.get("value")}]
-        elif parameters.get("id") == 1:
-            arr = [{"id": 1, "value": parameters.get("value")},
-                   {"id": 2, "value": start_balance // parameters.get("value")}]
+            parameters = [{"id": 2, "value": 5},
+                          {"id": 1, "value": start_balance // 5, "is_calc": True}]
+
+        elif parameters[0]["id"] == 2:
+            parameters.append({"id": 1, "value": start_balance // parameters[0]["value"], "is_calc": True})
+        elif parameters[0]["id"] == 1:
+            parameters.append({"id": 2, "value": start_balance // parameters[0]["value"], "is_calc": True})
         else:
             raise ValidationError("id в parameters должен принимать значение 1 или 2")
-        print('parameters:', arr)
-
-        if name is None or name == "":
-            raise ValidationError("Не передан параметр name ")
+        validated_data['parameters'] = parameters
+        print('parameters:', parameters)
 
         created_challenge_instance = super().create(validated_data)
 
