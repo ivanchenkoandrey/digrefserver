526ca1816e1ef4cbca7390add65eede67c79c285
Andrey Ivanchenko <varioustemp@gmail.com>
2022-10-24 12:58:13 +0300

    workers amount increased

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,5 +1,8 @@
+import multiprocessing
+
 bind = '0.0.0.0:8888'
-workers = 4
+workers = multiprocessing.cpu_count() * 2 + 1
+threads = 2
 loglevel = 'debug'
 accesslog = '-'
 access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" %(L)s'
